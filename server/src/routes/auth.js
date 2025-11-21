4eb68ce253edcd135e3884087490f299a6875dbe
Syed saquib <syedssaquib121@gmail.com>
2025-11-22 02:12:00 +0530

    fix import path / rename User.js

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
-import User from "../models/User.js";
+import User from "../models/user.js";
 import { createAccessToken, createRefreshToken } from "../utils/tokens.js";
 import { sendReminderEmail } from "../utils/mailer.js";
 
